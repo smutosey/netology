fa67c6d3a8af529ff3fd72ba98d7aa6626ec3a1e
Nedorezov Alexander <alserned@gmail.com>
2022-09-23 02:22:41 +0700

    Задание выполнено:  bash/5.01 - basics/task2.sh

--- 5. bash/5.01 - basics/task2.sh
+++ 5. bash/5.01 - basics/task2.sh
@@ -1,1 +1,7 @@
 # Напишите скрипт, который каждые 5 секунд будет выводить на экран текущее время и содержимое файла /proc/loadavg.
+#!/bin/bash
+while :
+do
+	date +'%R' && cat /proc/loadavg
+    sleep 5
+done
