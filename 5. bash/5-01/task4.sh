e40808649f1805db8ae9acb0ee144b88ac6dd50e
Nedorezov Alexander <alserned@gmail.com>
2022-09-24 15:20:08 +0700

    done: 5. bash/5-01/task4.sh

--- 5. bash/5-01/task4.sh
+++ 5. bash/5-01/task4.sh
@@ -3,45 +3,33 @@
 # Считывание параметров реализуйте с помощью read и select.
 # Примечение: постарайтесь максимально защититься от ошибок, т.к. пользователи любят написать строку вместо числа.
 
-
-
-calc () {
-  read -p "Введите первое число: " n1
-  read -p "Введите второе число: " n2
-  echo "$n1 $1 $n2 = " $(bc -l <<< "$n1$1$n2")
+check () {
+  case $1 in
+    *[!0-9]* | "") #не число или пустая строка
+      echo -e "\tОшибка: Введите число."
+      return 1;;
+    *)
+      return 0;;
+  esac
 }
 
-echo "Выберите операцию: "
-while true do 
-    select opt in "+" "-" "*" "/" do 
-        calc $opt';
-    done
+calc () {
+  read -p "Введите первое число: " num1
+  check $num1 || return $?
+  read -p "Введите второе число: " num2
+  check $num2 || return $?
+  echo "$num1 $1 $num2 = " $(bc -l <<< "$num1$1$num2")
+}
+
+PS3="Выберите операцию (по номеру): "
+select opt in "+" "-" "*" "/" "Выход"; do 
+  case $opt in 
+    ("+" | "-" | "*" | "/")
+      calc "$opt";;
+    "Выход")
+      break;;
+    *)
+      echo -e "\tОшибка: Неправильный номер операции $REPLY";;
+  esac
 done
 
-
-
-# calculate () {
-#   read -p "Введите первое число: " n1
-#   read -p "Введите второе число: " n2
-#   echo "$n1 $1 $n2 = " $(bc -l <<< "$n1$1$n2")
-# }
-
-# PS3="Выберите операцию: "
-
-# select opt in add subtract multiply divide quit; do
-
-#   case $opt in
-#     add)
-#       calculate "+";;
-#     subtract)
-#       calculate "-";;
-#     multiply)
-#       calculate "*";;
-#     divide)
-#       calculate "/";;
-#     quit)
-#       break;;
-#     *) 
-#       echo "Недопустимая опция $REPLY";;
-#   esac
-# done
